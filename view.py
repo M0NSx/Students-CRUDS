115cce4fddab8c80818802d27aada850aa6bffd4
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-10 16:56:07 -0300

    Update view.py

--- view.py
+++ view.py
@@ -4,3 +4,4 @@ try:
   banque = sqlite3.connect('etudiant_register.db')
   print("Data base été connecter avec succès")
 except sqlite3.Error as erreur:
+  print("Il n'a pas été possible de se connecter dans data base", erreur)
