4acab54c202c9b510e022a673764e0bcf39bc885
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-12 17:41:04 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog as fd
 
+from PIL import ImageTk, Image
+
 # couleurs
 co0 = "#2e2d2b"  # noir
 co1 = "#feffff"  # blanc 
