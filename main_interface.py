b63d4fad6961ab7011ce934c2db6f6ca598638e7
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-13 17:36:17 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -130,6 +130,7 @@ def Ajouter():
     l_ligne.place(x=372, y=10)
 
     l_ligne = Label(frame_tableau_ligne, relief=GROOVE, text="h", height=100, anchor=NW, font=("Ivy 1"), bg=co0, fg=co0)
+    l_ligne.place(x=6, y=10)
 
 def Sauver():
     print("Sauver")
