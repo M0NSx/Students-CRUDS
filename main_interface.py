d2b4032f03ce68381990e34ad6c07c6a08f1b466
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-13 17:22:21 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -65,6 +65,7 @@ def Ajouter():
 
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
+    e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
 
 def Sauver():
     print("Sauver")
