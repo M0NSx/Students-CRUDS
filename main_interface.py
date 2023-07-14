89b65d175fede8dd9795101153887fb6c92e00af
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-14 19:08:19 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -66,6 +66,7 @@ def Ajouter():
     frame_tableau_classe = Frame(frame_tableau, width=300, height=200, bg=co1)
     frame_tableau_classe.grid(row=0, column=2, pady=0, padx=10, sticky=NSEW)
 
+    #détails du cours
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
