f5e8a7ffcc4faf9922773069f40cba5b4713d466
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-14 19:09:27 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -143,6 +143,7 @@ def Ajouter():
     l_nom = Label(frame_détails, text="Nom du classe", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=404, y=10)
     e_nom_classe = Entry(frame_détails, width=35, justify="left", relief="solid")
+    e_nom_classe.place(x=407, y=40)
 
 
 def Sauver():
