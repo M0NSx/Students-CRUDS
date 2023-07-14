bee715ac77e290c499de9eabab08f0d03f510cfd
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-14 19:04:31 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -63,7 +63,7 @@ def Ajouter():
     frame_tableau_ligne = Frame(frame_tableau, width=30, height=200, bg=co1)
     frame_tableau_ligne.grid(row=0, column=1, pady=0, padx=10, sticky=NSEW)
 
-    frame_tableau_classe = Frame(frame_tableau, width=300, height=200, bg=co4)
+    frame_tableau_classe = Frame(frame_tableau, width=300, height=200, bg=co1)
     frame_tableau_classe.grid(row=0, column=2, pady=0, padx=10, sticky=NSEW)
 
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
