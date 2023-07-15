a57fcefd2b99f97baeb4e56931ea92c34c837936
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-15 16:23:12 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -81,6 +81,7 @@ def Étudiants():
     date_naissance.place(x=450, y=40)
 
     l_cpf = Label(frame_détails, text="CPF", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
+    l_cpf.place(x=446, y=70)
 
 def Ajouter():
     frame_tableau_cours = Frame(frame_tableau, width=300, height=200, bg=co1)
