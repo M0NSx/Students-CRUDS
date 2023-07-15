7bfbf17e1fe8c378082bdb27854e718227fbfca9
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-15 16:21:20 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -75,6 +75,8 @@ def Étudiants():
     c_sexe['values'] = ("Masculin","Feminine")
     c_sexe.place(x=190, y=160)
 
+    l_date_naissance = Label(frame_détails, text="Date de naissance", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
+
 def Ajouter():
     frame_tableau_cours = Frame(frame_tableau, width=300, height=200, bg=co1)
     frame_tableau_cours.grid(row=0, column=0, pady=0, padx=10, sticky=NSEW)
