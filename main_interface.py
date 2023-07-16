86b7775fbd920f02eaff61f0ea62cd1ce5528f4f
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-16 16:17:01 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -159,6 +159,7 @@ def Étudiants():
 
         tree_étudiants.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
         tree_étudiants.grid(column=0, row=1, sticky='nsew')
+        vsb.grid(column=1, row=1, sticky='ns')
 
 def Ajouter():
     frame_tableau_cours = Frame(frame_tableau, width=300, height=200, bg=co1)
