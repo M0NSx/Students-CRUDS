8c6bf2f8c0123dc7fb564860dc2e43d29dd7cba5
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-16 16:11:26 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -123,6 +123,7 @@ def Étudiants():
     l_nom1 = Label(frame_détails, text="Rechercher étudiant", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom1.place(x=627, y=10)
     e_nom_rechercher = Entry(frame_détails, width=17, justify="center", relief="solid", font=("Ivy 10"))
+    e_nom_rechercher.place(x=630, y=35)
 
 
 def Ajouter():
