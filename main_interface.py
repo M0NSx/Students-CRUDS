af0b957e2bc168214740dd6f00e0f5e2108fe2a4
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-16 16:15:23 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -150,7 +150,9 @@ def Étudiants():
         df_list = []
 
         global tree_étudiants
-        
+
+        tree_étudiants = ttk.Treeview(frame_tableau, selectmode="extended",columns=list_header, show="headings")
+
 def Ajouter():
     frame_tableau_cours = Frame(frame_tableau, width=300, height=200, bg=co1)
     frame_tableau_cours.grid(row=0, column=0, pady=0, padx=10, sticky=NSEW)
