0535e57bcc84bfd75f8c187a7654cea7f101a47f
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:37:15 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -251,6 +251,8 @@ def Ajouter():
 
             bouton_sauver3 = Button(frame_détails, command=update, anchor=CENTER, text="Sauver".upper(), width=10, overrelief=RIDGE, font=("Ivy 7"), bg=co3, fg=co1)
             bouton_sauver3.place(x=227, y=130)
+        except IndexError:
+            messagebox.showerror('Erreur',"Sélectionnez l'un des cours du tableau")
 
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
