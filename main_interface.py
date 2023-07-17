987984da2cbcc557bb9027ecd89a59c7f6eab960
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:27:31 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -219,6 +219,8 @@ def Ajouter():
             tree_dictionnaire = tree_cours.item(tree_itens)
             tree_liste = tree_dictionnaire['values']
 
+            valeur_id = tree_liste[0]
+
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
