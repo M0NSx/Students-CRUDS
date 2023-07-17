6591e06b17930798f2684216c8e1f995eb4e318f
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:27:48 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -221,6 +221,8 @@ def Ajouter():
 
             valeur_id = tree_liste[0]
 
+            e_nom_cours.insert(0, tree_liste[1])
+
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
