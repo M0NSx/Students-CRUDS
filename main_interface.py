becc7b57123bd86a36010ee6c6e6abd2ca9359d8
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:30:54 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -232,6 +232,8 @@ def Ajouter():
 
                 liste = [nom, durée, prix, valeur_id]
 
+                for i in liste:
+
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
