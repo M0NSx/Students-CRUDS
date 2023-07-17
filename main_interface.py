a162c9b3d159b4c682fe862e5e91ecf40a68fa18
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:29:49 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -227,6 +227,7 @@ def Ajouter():
 
             def update():
                 nom = e_nom_cours.get()
+                durée = e_durée.get()
 
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
