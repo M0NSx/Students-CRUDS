cf979958a79dd23bdf91b0a5f536eea3f1c0cbbc
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:32:35 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -239,6 +239,8 @@ def Ajouter():
 
                 update_cours(liste)
 
+                messagebox.showinfo("Succès", "les données ont été insérées avec succès!")
+
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
