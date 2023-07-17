da1a4e06f916c56112742baeb626bfcb2acc668a
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:24:09 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -205,6 +205,8 @@ def Ajouter():
 
         créer_cours(liste)
 
+        messagebox.showinfo("Succès", "Tu rempli tout avec succès!")
+
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
