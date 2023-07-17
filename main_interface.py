67639e34a48e0d7a640b3ef0b1751040e1461aa1
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:23:50 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -203,6 +203,8 @@ def Ajouter():
                 messagebox.showerror('Erreur', "Il faut remplir l'espace")
                 return
 
+        créer_cours(liste)
+
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
