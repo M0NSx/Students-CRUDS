6f836a631f87bec3dca8896f9c6229fd3516a5f1
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-17 18:26:36 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -215,7 +215,8 @@ def Ajouter():
 
     def actualiser_cours():
         try:
-            
+            tree_itens = tree_cours.focus()
+
     l_nom = Label(frame_détails, text="Nom du cours", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
     e_nom_cours = Entry(frame_détails, width=35, justify="left", relief="solid")
