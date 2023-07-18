ed49b58253a1893a93c9683c482c006f85b3d2f8
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-18 15:24:00 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -374,6 +374,8 @@ def Ajouter():
 
             valeur_id = tree_liste[0]
 
+            e_nom_classe.insert(0, tree_liste[1])
+
     l_nom = Label(frame_détails, text="Nom du classe", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=404, y=10)
     e_nom_classe = Entry(frame_détails, width=35, justify="left", relief="solid")
