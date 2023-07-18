808213a9a1d37cf7885f8e17f18eaa0fc30d48bb
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-18 15:33:14 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -392,6 +392,8 @@ def Ajouter():
 
                 update_classes(liste)
 
+                messagebox.showinfo("Succès", "les données ont été insérées avec succès!")
+
     l_nom = Label(frame_détails, text="Nom du classe", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=404, y=10)
     e_nom_classe = Entry(frame_détails, width=35, justify="left", relief="solid")
