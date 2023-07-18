be9f70eb70f0a0998e0e35caabc8bf7102cdba8b
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-18 15:32:25 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -390,6 +390,8 @@ def Ajouter():
                         messagebox.showerror('Erreur', "Il faut remplir l'espace")
                         return
 
+                update_classes(liste)
+
     l_nom = Label(frame_détails, text="Nom du classe", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=404, y=10)
     e_nom_classe = Entry(frame_détails, width=35, justify="left", relief="solid")
