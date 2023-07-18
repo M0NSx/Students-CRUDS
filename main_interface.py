75d266c6cbd770075d1efe167b29570a079526e0
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-18 15:30:30 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -386,6 +386,7 @@ def Ajouter():
                 liste = [nom, cours, date, valeur_id]
 
                 for i in liste:
+                    if i == "":
 
     l_nom = Label(frame_détails, text="Nom du classe", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=404, y=10)
