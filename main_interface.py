bbfa7bec116a7c2ba863ae9729c61fc4df8bc2c1
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-19 17:19:25 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -65,6 +65,7 @@ def Étudiants():
         sexe = c_sexe.get()
         naissance = date_naissance.get()
         cpf = e_cpf.get()
+        cours = c_classe.get()
 
     
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
