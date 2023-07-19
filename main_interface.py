f1c835f4c30aa898e2d9c64d831841b0850447e6
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-19 17:17:53 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -57,6 +57,7 @@ app_lg.place(x=0, y=0)
 
 def Étudiants():
     def nouveu_étudiants():
+        global image, image_string, l_image
 
     
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
