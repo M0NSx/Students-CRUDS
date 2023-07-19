cd92653787ea7bad570831b29c9ef138b7d5eb15
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-19 17:22:23 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -81,6 +81,7 @@ def Étudiants():
 
         e_nom.delete(0,END)
         e_email.delete(0,END)
+        c_sexe.delete(0,END)
 
     
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
