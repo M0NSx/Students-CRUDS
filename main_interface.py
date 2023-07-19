468894129ff934b68a1103717b2b99fe542f2209
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-19 17:21:53 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -79,6 +79,8 @@ def Étudiants():
 
         messagebox.showinfo("Succès", "Tu rempli tout avec succès!")
 
+        e_nom.delete(0,END)
+
     
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
