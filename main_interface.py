599dd584e0eea5da60f642e9170f14e645926d98
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-20 21:39:14 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -101,6 +101,8 @@ def Étudiants():
             
             valeur_id = tree_liste[0]
 
+            e_nom.delete(0,END)
+
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
