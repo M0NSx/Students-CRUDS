14daae36cba5c48326cac45d8f477e6c19b9168a
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-20 21:42:33 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -117,6 +117,8 @@ def Étudiants():
             e_cpf.insert(0,tree_liste[7])
             c_classe.insert(0,tree_liste[8])
 
+            image = tree_liste[5]
+
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
