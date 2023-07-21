31aec74b4998fbe5e347e1e67a83c22e7bb9e012
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-20 21:43:24 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -121,6 +121,7 @@ def Étudiants():
             image_string = image
 
             image = Image.open(image)
+            image = image.resize((130,130))
 
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
