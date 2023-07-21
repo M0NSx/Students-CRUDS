9e719caa2b97d43d23ab279ee94b08ee14452925
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-20 21:44:05 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -124,6 +124,7 @@ def Étudiants():
             image = image.resize((130,130))
             image = ImageTk.PhotoImage(image)
             l_image = Label(frame_détails, image=image, bg=co1, fg=co4)
+            l_image.place(x=300, y=10)
 
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
