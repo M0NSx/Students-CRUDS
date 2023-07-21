3d74a9377a730a9ee890f644b16853c704628b6c
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-20 21:41:50 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -113,6 +113,7 @@ def Étudiants():
             e_email.insert(0,tree_liste[2])
             e_téléphone.insert(0,tree_liste[3])
             c_sexe.insert(0,tree_liste[4])
+            date_naissance.insert(0,tree_liste[6])
 
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
