cc4aaf8ed1a4dc72c36f4d876e379414304d8f10
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-21 21:01:27 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -177,6 +177,8 @@ def Étudiants():
 
             messagebox.showinfo("Succès", "Vous avez supprimé l'étudiant avec succès!")
 
+            montrer_étudiants()
+
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
