03d0348922ba835ee486019cc2543f9883ab902f
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-21 21:00:25 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -169,6 +169,7 @@ def Étudiants():
         try:
             tree_itens = tree_étudiants.focus()
             tree_dictionnaire = tree_étudiants.item(tree_itens)
+            tree_liste = tree_dictionnaire['values']
 
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
