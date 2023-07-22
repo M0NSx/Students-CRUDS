c78082a687a9e6437db70e35a78c2bbbf07ce1d3
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-07-21 21:01:44 -0300

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -179,6 +179,8 @@ def Étudiants():
 
             montrer_étudiants()
 
+        except IndexError:
+
     #Création d'entrées
     l_nom = Label(frame_détails, text="Nom", height=1, anchor=NW, font=("Ivy 10"), bg=co1, fg=co4)
     l_nom.place(x=4, y=10)
